1ba0749622f0993368d08f44e079e50d06ba1edc
Roberto C. Pereira <roberto.maranata@gmail.com>
2020-06-18 22:48:47 -0300

    inicio

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Id is not valid" });
+  }
+  const repositoryIndex = repositories.findIndex((rep) => rep.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Id not found!" });
+  }
+  next();
+}
+app.use("/repositories/:id", validateId);
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex((rep) => rep.id === id);
+  const repository = repositories[repositoryIndex];
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex((rep) => rep.id === id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex((rep) => rep.id === id);
+  const repository = repositories[repositoryIndex];
+  repository.likes = repository.likes + 1;
+  return response.json(repository);
+});
+
+module.exports = app;
